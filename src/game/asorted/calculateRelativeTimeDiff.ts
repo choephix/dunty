35aa2bae4a8fd60d13d172a511034ad47716d76b
Stefan Ginev <choephix@gmail.com>
2022-05-22 17:29:34 +0000

    -

--- src/game/asorted/calculateRelativeTimeDiff.ts
+++
@@ -1,9 +0,0 @@
-export function calculateRelativeTimeDiffFromNow(epochSeconds: number, absolute: boolean = true) {
-  const nowSeconds = new Date().getTime() * 0.001;
-  const diffSeconds = nowSeconds - epochSeconds;
-  if (absolute) {
-    return Math.abs(diffSeconds);
-  } else {
-    return diffSeconds;
-  }
-}
--- src/game/asorted/digRewardAmmountFromTransactionResult.ts
+++
@@ -1,8 +0,0 @@
-import { findTransactionActionTrace } from "@sdk-integration/contracts/utils/findTransactionActionTrace";
-import { SignTransactionResponse } from "universal-authenticator-library";
-
-export function digRewardAmmountFromTransactionResult(result: SignTransactionResponse) {
-  const trace_Transfer = findTransactionActionTrace.byName(result.transaction, "transfer");
-  const trace_TransferData = trace_Transfer?.act.data as { memo: string; quantity: string } | null;
-  return trace_TransferData?.quantity || null;
-}
