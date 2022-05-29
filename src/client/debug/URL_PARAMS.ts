e656a8b7b9f32a663bb8315a00dc5e0d7f0d95e8
Stefan Ginev <choephix@gmail.com>
2022-05-29 12:34:29 +0000

    -

--- src/client/debug/URL_PARAMS.ts
+++ src/client/debug/URL_PARAMS.ts
@@ -1,4 +1,4 @@
 const params = new URLSearchParams(window.location.search);
 
 export const __VERBOSE__ = !! params.get('verbose');
-export const __FLOOR__ = Number(params.get('floor') || 1);
+export const __FLOOR__ = Number(params.get('floor') || Number(/\/(\d+)/ig.exec(location.pathname)?.[1]) || 1);
