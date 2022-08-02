59c999a95c6d90a718547eee498c5382b5980b4e
Stefan Ginev <choephix@gmail.com>
2022-08-02 07:53:54 +0000

    -

---
+++ src/surface/getWorldGridNaturalData.ts
@@ -0,0 +1,57 @@
+import { range2D } from "@sdk/math/range2d";
+import { randomIntBetweenIncluding } from "@sdk/utils/random";
+
+const BASE = "https://public.cx/mock/surfaces/";
+const TILES = [
+  `023b60bbc3877990a3b1ff4d805513a8.jpg`, // grass
+  `c412de6208fca6ab77ad117eb74c6387.jpg`, // grass dk
+  `af8753c5bbbb6310baad7b8f8d8847e1.jpg`, // rock
+  `3767d2aaa55ae0afe5db1b75683293de.jpg`, // magical blue rock
+  `18cbfcd1190b26645d81b0b25eac198e.jpg`, // lava-ish
+  `bfec9c57d9f67f5d34336323a38aca41.jpg`, // alien cheese
+];
+
+interface TileNaturalData {
+  gridPositionX: number;
+  gridPositionY: number;
+  textureUrl: string;
+  textureFileName: string;
+}
+
+export function getWorldGridNaturalData() {
+  const tiles = new Array<TileNaturalData>();
+
+  const GROUP_SIZE = 5;
+  const GR = Math.floor(GROUP_SIZE / 2);
+
+  for (let [gx, gy] of range2D.fromToIncluding(-4, -4, 4, 4)) {
+    if (gx === 0 && gy === 0) {
+      for (let [dx, dy] of range2D.fromToIncluding(-GR, -GR, GR, GR)) {
+        const file = `79780337e003fd1cab5db4775c5516f8.jpg`;
+        const tile = {
+          gridPositionX: gx * GROUP_SIZE + dx,
+          gridPositionY: gy * GROUP_SIZE + dy,
+          textureUrl: BASE + file,
+          textureFileName: file,
+        };
+        tiles.push(tile);
+      }
+    } else {
+      const variety = 2;
+      const rand = randomIntBetweenIncluding(0, TILES.length - variety, 4);
+      const tilePool = TILES.slice(rand, rand + variety);
+      for (let [dx, dy] of range2D.fromToIncluding(-GR, -GR, GR, GR)) {
+        const file = tilePool[randomIntBetweenIncluding(0, tilePool.length - 1, 4)];
+        const tile = {
+          gridPositionX: gx * GROUP_SIZE + dx,
+          gridPositionY: gy * GROUP_SIZE + dy,
+          textureUrl: BASE + file,
+          textureFileName: file,
+        };
+        tiles.push(tile);
+      }
+    }
+  }
+
+  return tiles;
+}
--- src/surface/initializeSurfaceWorld.ts
+++ src/surface/initializeSurfaceWorld.ts
@@ -6,17 +6,8 @@ import { range2D } from "@sdk/math/range2d";
 import { randomInt, randomIntBetweenIncluding } from "@sdk/utils/random";
 import { gsap } from "gsap";
 import { Viewport } from "pixi-viewport";
+import { getWorldGridNaturalData } from "./getWorldGridNaturalData";
 import { Tile, TileHolo, TILE_SIZE } from "./tiles";
-
-const BASE = "https://public.cx/mock/surfaces/";
-const TILES = [
-  `023b60bbc3877990a3b1ff4d805513a8.jpg`, // grass
-  `c412de6208fca6ab77ad117eb74c6387.jpg`, // grass dk
-  `af8753c5bbbb6310baad7b8f8d8847e1.jpg`, // rock
-  `3767d2aaa55ae0afe5db1b75683293de.jpg`, // magical blue rock
-  `18cbfcd1190b26645d81b0b25eac198e.jpg`, // lava-ish
-  `bfec9c57d9f67f5d34336323a38aca41.jpg`, // alien cheese
-];
 
 export function initializeSurfaceWorld(app: Application) {
   console.log("Initializing surface world...");
@@ -26,48 +17,18 @@ export function initializeSurfaceWorld(app: Application) {
   viewport.position.set(window.innerWidth / 2, window.innerHeight / 2);
   app.stage.addChild(viewport);
 
+  const tilesData = getWorldGridNaturalData();
   const tiles = new Array<Tile>();
 
-  const GROUP_SIZE = 5;
-  const GR = Math.floor(GROUP_SIZE / 2);
+  for (const tileData of tilesData) {
+    const tile = new Tile(tileData.textureUrl);
+    viewport.addChild(tile);
 
-  for (let [gx, gy] of range2D.fromToIncluding(-4, -4, 4, 4)) {
-    if (gx === 0 && gy === 0) {
-      for (let [dx, dy] of range2D.fromToIncluding(-GR, -GR, GR, GR)) {
-        const file = `79780337e003fd1cab5db4775c5516f8.jpg`;
-        const tile = new Tile(BASE + file);
-        viewport.addChild(tile);
+    const p = worldToIsometricPosition(tileData.gridPositionX, tileData.gridPositionY);
+    tile.position.set(p.x * TILE_SIZE, p.y * TILE_SIZE);
+    tile.textureFileName = tileData.textureFileName;
 
-        const ix = gx * GROUP_SIZE + dx;
-        const iy = gy * GROUP_SIZE + dy;
-        const p = worldToIsometricPosition(ix, iy);
-        tile.isDungeonTile = true;
-        tile.position.set(p.x * TILE_SIZE, p.y * TILE_SIZE);
-        tile.zIndex = tile.x + tile.y * 10;
-
-        tiles.push(tile);
-      }
-    } else {
-      const variety = 2;
-      const rand = randomIntBetweenIncluding(0, TILES.length - variety, 4);
-      const tilePool = TILES.slice(rand, rand + variety);
-
-      for (let [dx, dy] of range2D.fromToIncluding(-GR, -GR, GR, GR)) {
-        const file = tilePool[randomIntBetweenIncluding(0, tilePool.length - 1, 4)];
-        const tile = new Tile(BASE + file);
-        viewport.addChild(tile);
-
-        const ix = gx * GROUP_SIZE + dx;
-        const iy = gy * GROUP_SIZE + dy;
-        const p = worldToIsometricPosition(ix, iy);
-        tile.position.set(p.x * TILE_SIZE, p.y * TILE_SIZE);
-        tile.zIndex = tile.x + tile.y * 10;
-
-        tiles.push(tile);
-
-        tile.textureFileName = file;
-      }
-    }
+    tiles.push(tile);
   }
 
   const dungeon = Sprite.from("https://public.cx/dunty/dungeon.png");
