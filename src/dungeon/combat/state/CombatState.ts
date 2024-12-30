c63f62587e10c6370d5862a8fd5e6a8efb417ad1
Stefan Ginev <stefan@revoltage.io>
2024-12-30 21:52:51 +0200

    Refactor Combatant constructor and add random Pokémon sprite URL function
    - Updated the Combatant constructor to accept a characterId parameter, enhancing flexibility in character initialization.
    - Replaced the static textureId assignment with a new function, getRandomPokemonSpriteUrl, which dynamically generates a Pokémon sprite URL.
    - This change improves the variability of combatant appearances and maintains code clarity by encapsulating sprite URL logic in a dedicated function.

--- src/dungeon/combat/state/CombatState.ts
+++ src/dungeon/combat/state/CombatState.ts
@@ -120,9 +120,12 @@ export class Combatant {
     return this.status.health > 0;
   }
 
-  constructor(initialStatus: Partial<CombatantStatus> = {}, characterId = getRandomItemFrom(COMBATANT_TEXTURES_LOOKING_RIGHT)) {
+  constructor(
+    initialStatus: Partial<CombatantStatus> = {},
+    characterId: string
+  ) {
     this.characterId = characterId;
-    this.textureId = `https://undroop-assets.web.app/enigma/sugimori/${this.characterId}.png`;
+    this.textureId = getRandomPokemonSpriteUrl();
 
     Object.assign(this.status, initialStatus);
   }
@@ -175,4 +178,15 @@ export enum CardPileType {
   HAND = "HAND",
   DISCARD = "DISCARD",
   VOID = "VOID",
+}
+
+function getRandomPokemonSpriteUrl() {
+  const useUndroopAssets = true;
+  if (useUndroopAssets) {
+    const characterId = getRandomItemFrom(COMBATANT_TEXTURES_LOOKING_RIGHT);
+    return `https://undroop-assets.web.app/enigma/sugimori/${characterId}.png`;
+  }
+
+  const num = Math.floor(Math.random() * 1025);
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${num}.png`;
 }
